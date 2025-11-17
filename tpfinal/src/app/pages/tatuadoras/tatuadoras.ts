768d4f05e2f1470c98cabdff17537fdf92c0e9e9
martitaperez <martiperezi99@gmail.com>
2025-11-17 16:19:31 -0300

    pages

---
+++ tpfinal/src/app/pages/tatuadoras/tatuadoras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tatuadoras',
+  imports: [],
+  templateUrl: './tatuadoras.html',
+  styleUrl: './tatuadoras.css',
+})
+export class Tatuadoras {
+
+}
---
+++ tpfinal/src/app/pages/turnos/turnos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-turnos',
+  imports: [],
+  templateUrl: './turnos.html',
+  styleUrl: './turnos.css',
+})
+export class Turnos {
+
+}
