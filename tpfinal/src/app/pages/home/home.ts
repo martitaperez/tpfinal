d7ae30da781cd6c0abca38f0dd625e3a11f23a54
CarlangasD <carlaaleduarte@gmail.com>
2025-11-26 23:27:29 -0300

    cambios en home e imagenes

--- tpfinal/src/app/pages/home/home.ts
+++ tpfinal/src/app/pages/home/home.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { ArtistService } from '../../services/artist.service';
+import { Artist } from '../../models/artist.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+  artistas: Artist[] = [];
+  artistaSeleccionada: Artist | null = null;
+
+  constructor(
+    private artistService: ArtistService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.artistService.getAll().subscribe(a => {
+      this.artistas = a;
+    });
+  }
+
+  verMas(artist: Artist) {
+    this.artistaSeleccionada = artist;
+  }
+
+  cerrarDetalle() {
+    this.artistaSeleccionada = null;
+  }
+
+  irAReservar() {
+    this.router.navigate(['/turnos']);
+  }
+}
